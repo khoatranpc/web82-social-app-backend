118e7ad0e2363f522540c7d425085752331131d1
khotranpc <khotranpc603@gmail.com>
2024-09-11 19:44:52 +0700

    Add cors

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ import express from "express";
 import dotenv from 'dotenv';
 import connectDatabase from "./database/index.js";
 import RootRouter from "./routes/index.js";
+import cors from 'cors';
 dotenv.config();
 
 const POST = process.env.PORT || 8080;
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 connectDatabase();
